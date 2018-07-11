417a742d545399a0d87a10b3f490d39c5580f337
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-11 20:44:15 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,10 @@
 class SecretsController < ApplicationController
   before_action :require_login
 
-  def index
-  end 
+  private
+
+    def require_login
+      return head(:forbidden) unless session[:name]   
+    end
+
 end
